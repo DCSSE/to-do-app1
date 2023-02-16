1182a912623ed3ca0b7b386c71b730eb685dd44a
alekab <aleka2002@gmail.com>
2023-02-16 16:36:11 +0600

    Using arrays as components

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ import SearchPan from "./srch-panel";
 import TodoList from "./todo-list";
 
 const App = () => {
+
+    const DataB = [
+        {label: 'Drink tea', important: false},
+        {label: 'Implement changes in app', important: true},
+        {label: 'Make notes', important: false},
+        {label: 'Buy groceries', important: false},
+
+    ];
+
+
     return <div>
         <AppHeader/>
         <SearchPan/>
-        <TodoList/>
+        <TodoList thtodo={DataB}/>
     </div>
 }
 
--- src/todo-list.js
+++ src/todo-list.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import TodoListItem from "./todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({thtodo}) => {
 
     return (
         <ul>
-            <li> <TodoListItem label= "Drink tea" /> </li>
-            <li> <TodoListItem label= "Implement changes in app"/>  </li>
-            <li> <TodoListItem label= "Make notes"/>  </li>
-            <li> <TodoListItem label= "Don't be lazy" important />  </li>
+            <li> <TodoListItem
+                label = {thtodo[0].label}
+            important={thtodo[0].important}/> </li>
+
+            <li> <TodoListItem
+                label = {thtodo[1].label}
+                important={thtodo[1].important}/> </li>
+
+            <li> <TodoListItem
+                label = {thtodo[2].label}
+                important={thtodo[2].important}/> </li>
+
+            <li> <TodoListItem
+                label = {thtodo[3].label}
+                important={thtodo[3].important}/> </li>
+
+
         </ul>
     )
 }
