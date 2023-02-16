6d8136c6dd91d845590e600d7df5aa9091cabdac
alekab <aleka2002@gmail.com>
2023-02-16 16:41:24 +0600

    Map method for arrays

--- src/todo-list.js
+++ src/todo-list.js
@@ -3,25 +3,17 @@ import TodoListItem from "./todo-list-item";
 
 const TodoList = ({thtodo}) => {
 
+    const elms = thtodo.map( (item) =>  {
+        return (
+            <li>
+                <TodoListItem label={item.label} important={item.important} />
+            </li>
+        );
+    });
+
     return (
         <ul>
-            <li> <TodoListItem
-                label = {thtodo[0].label}
-            important={thtodo[0].important}/> </li>
-
-            <li> <TodoListItem
-                label = {thtodo[1].label}
-                important={thtodo[1].important}/> </li>
-
-            <li> <TodoListItem
-                label = {thtodo[2].label}
-                important={thtodo[2].important}/> </li>
-
-            <li> <TodoListItem
-                label = {thtodo[3].label}
-                important={thtodo[3].important}/> </li>
-
-
+            {elms}
         </ul>
     )
 }
