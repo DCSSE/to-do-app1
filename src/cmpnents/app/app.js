ed2287759f774b600238d02391523f218a8bdb71
alekab <aleka2002@gmail.com>
2023-02-28 19:55:09 +0600

    step 25 deleting elements setState and deleteItem func.

--- src/cmpnents/app/app.js
+++ src/cmpnents/app/app.js
@@ -1,32 +1,66 @@
-import React from "react";
+import React, {Component} from "react";
 
 import AppHeader from "../app-header";
-import SearchPan from "../search-pan/srch-panel";
 import TodoList from "../todo-list/todo-list";
 import ItemStatusFilter from "../item-status-filter";
 import './app.css'
 import SrchPanel from "../search-pan/srch-panel";
 
-const App = () => {
-    const DataB = [
-        {label: 'Drink tea', important: false, id:1},
-        {label: 'Implement changes in app', important: true, id:2},
-        {label: 'Make notes', important: false, id:3},
-        {label: 'Buy groceries', important: false, id:4},
+export default class App extends Component {
 
-    ];
+    state = {
+        DataB: [
+            {label: 'Drink tea', important: false, id:1},
+            {label: 'Implement changes in app', important: true, id:2},
+            {label: 'Make notes', important: false, id:3},
+            {label: 'Buy groceries', important: false, id:4},
 
-    return (
-        <div className="todo-app">
-            <AppHeader toDo={1} done={3} />
-            <div className="top-panel d-flex">
-                <SrchPanel />
-                <ItemStatusFilter />
+        ]
+    };
+
+    deleteItem = (id) => {
+        this.setState( ({DataB}) => {
+            const idDel = DataB.findIndex( (elm) =>  elm.id === id );
+            console.log(idDel);
+            DataB.splice(id, 1);
+            // [a, b c, d, e]
+            // [a, b,   d, e]
+            const before = DataB.slice(0, idDel);
+            const after = DataB.slice(idDel+1);
+            const newArr = [...before, ...after];
+
+            return {
+                DataB: newArr
+            };
+        } );
+    };
+
+    render() {
+        return (
+            <div className="todo-app">
+                <AppHeader toDo={1} done={3} />
+
+                <div className="top-panel d-flex">
+                     <SrchPanel />
+                    <ItemStatusFilter />
+                </div>
+
+                <TodoList thtodo={this.state.DataB} onDeleted = { this.deleteItem} />
+
             </div>
+        );
+    }
 
-            <TodoList thtodo={DataB} onDeleted = {(id) => console.log('del', id) } />
-        </div>
-    )
+
+
+        // <div className="todo-app">
+        //     <AppHeader toDo={1} done={3} />
+        //     <div className="top-panel d-flex">
+        //         <SrchPanel />
+        //         <ItemStatusFilter />
+        //     </div>
+        //
+        //     <TodoList thtodo={DataB} onDeleted = {(id) => console.log('del', id) } />
+        // </div>
+
 }
-
-export default App;
